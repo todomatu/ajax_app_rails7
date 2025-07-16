24ee91894d75c2c95674794e4b219010b8e2aebf
todomatu <macbook.nk96@gmail.com>
2025-07-17 02:52:28 +0900

    メモ投稿機能
    コントローラーにリクエストを送信する。

---
+++ app/javascript/memo.js
@@ -0,0 +1,12 @@
+function post(){
+  const form = document.getElementById("form");
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const formData = new FormData(form);
+    const XHR = new XMLHttpRequest();
+    XHR.open("post","/posts",true);
+    XHR.responseType = "json" ;
+    XHR.send(formData);
+  });
+};
+window.addEventListener('load',post);
